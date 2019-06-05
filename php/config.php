db11c57b09017bc346342bce23d29c857cb2be08
Dandrew <43170355+Dandrew98@users.noreply.github.com>
2019-06-05 15:32:28 +0200

    weer aangepast config

--- php/config.php
+++ php/config.php
@@ -8,4 +8,9 @@ $db_database = 'vampire';
 // maak de database-verbinding
 $mysqli = mysqli_connect($db_hostname, $db_username, $db_password, $db_database);
 
+if ($mysqli === false)
+{
+  die("ERROR: Kan niet verbinden.". mysqli_connect_error());
+}
+
 ?>
